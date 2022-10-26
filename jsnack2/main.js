e509e156b29835df7c2b8622ebfdcdd0b8315f38
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-26 10:52:32 +0200

    first commit

---
+++ jsnack2/main.js
@@ -0,0 +1,8 @@
+const word1=prompt("Inserisci la prima parola")
+const word2=prompt("Inserisci la seconda parola")
+
+if (word1.length>word2.length) {
+    console.log(word1, word2)
+}else{
+    console.log(word2, word1)
+}
