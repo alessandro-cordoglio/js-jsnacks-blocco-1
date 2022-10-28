ba241189230148a79de0fd3fee7d018d89389c9a
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-28 10:41:02 +0200

    Snack-6

---
+++ jsnack6/main.js
@@ -0,0 +1,7 @@
+/* Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari */
+const arraynum=[1,2,3,4,5]
+let sum=0
+for (let i = 0; i < arraynum.length; i++) {
+        sum= sum+arraynum[i%2==1]
+}
+console.log(sum)
